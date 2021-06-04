3c6af6ad525f08f0dafcd915f9fa6b61ff226b4c
ryowuandjanet <ryowudata@gmail.com>
2021-06-04 09:58:17 +0800

    20210604-Build Your Own Confirm Dialog & Notification Material UI(Ch07)

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -11,6 +11,8 @@ import AddIcon from '@material-ui/icons/Add';
 import Popup from "../../components/Popup";
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import CloseIcon from '@material-ui/icons/Close';
+import Notification from "../../components/Notification";
+import ConfirmDialog from "../../components/ConfirmDialog";
 
 const useStyles = makeStyles(theme => ({
   pageContent: {
@@ -42,6 +44,8 @@ export default function Employees() {
   const [records, setRecords] = useState(employeeService.getAllEmployees())
   const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
   const [openPopup, setOpenPopup] = useState(false)
+  const [notify, setNotify] = useState({ isOpen: false, message: '', type: '' })
+  const [confirmDialog, setConfirmDialog] = useState({ isOpen: false, title: '', subTitle: '' })
 
   const {
     TblContainer,
@@ -71,11 +75,30 @@ export default function Employees() {
     setRecordForEdit(null)
     setOpenPopup(false)
     setRecords(employeeService.getAllEmployees())
+    setNotify({
+      isOpen: true,
+      message: 'Submitted Successfully',
+      type: 'success'
+    })
   }
 
   const openInPopup = item => {
     setRecordForEdit(item)
     setOpenPopup(true)
+  }
+
+  const onDelete = id => {
+    setConfirmDialog({
+      ...confirmDialog,
+      isOpen: false
+    })
+    employeeService.deleteEmployee(id);
+    setRecords(employeeService.getAllEmployees())
+    setNotify({
+      isOpen: true,
+      message: 'Deleted Successfully',
+      type: 'error'
+    })
   }
 
   return (
@@ -123,7 +146,15 @@ export default function Employees() {
                       <EditOutlinedIcon fontSize="small" />
                     </Controls.ActionButton>
                     <Controls.ActionButton
-                      color="secondary">
+                      color="secondary"
+                      onClick={() => {
+                        setConfirmDialog({
+                          isOpen: true,
+                          title: 'Are you sure to delete this record?',
+                          subTitle: "You can't undo this operation",
+                          onConfirm: () => { onDelete(item.id) }
+                        })
+                      }}>
                       <CloseIcon fontSize="small" />
                     </Controls.ActionButton>
                   </TableCell>
@@ -143,6 +174,14 @@ export default function Employees() {
           recordForEdit={recordForEdit}
           addOrEdit={addOrEdit} />
       </Popup>
+      <Notification
+        notify={notify}
+        setNotify={setNotify}
+      />
+      <ConfirmDialog
+        confirmDialog={confirmDialog}
+        setConfirmDialog={setConfirmDialog}
+      />
     </>
   )
 }
